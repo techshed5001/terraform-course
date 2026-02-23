5ef0e5c46d6776b7bed951676726befb0c5de45b
techshed5001 <charlie.long@yahoo.com>
2026-02-23 12:03:18 -0600

    added chapter 3 post apply

--- 03-basics/aws-backend/main.tf
+++ 03-basics/aws-backend/main.tf
@@ -4,13 +4,13 @@ terraform {
   ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
   ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
   #############################################################
-  # backend "s3" {
-  #   bucket         = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
-  #   key            = "03-basics/import-bootstrap/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-state-locking"
-  #   encrypt        = true
-  # }
+   backend "s3" {
+     bucket         = "devops-directive-tf-state-cwl" # REPLACE WITH YOUR BUCKET NAME
+     key            = "03-basics/terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-state-locking-cwl"
+     encrypt        = true
+   }
 
   required_providers {
     aws = {
@@ -25,7 +25,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
+  bucket        = "devops-directive-tf-state-cwl" # REPLACE WITH YOUR BUCKET NAME
   force_destroy = true
 }
 
@@ -46,7 +46,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
+  name         = "terraform-state-locking-cwl"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
